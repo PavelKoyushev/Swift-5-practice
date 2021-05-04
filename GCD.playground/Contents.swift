c35456440f4c2b3d66e8e610c04d9bc6def9113c
pkoyushev <pkoyushev@gmail.com>
2021-05-04 15:13:58 +0300

    add gcd

---
+++ GCD.playground/Contents.swift
@@ -0,0 +1,39 @@
+
+import UIKit
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+// Блоки отправки
+
+let workItem = DispatchWorkItem(qos: .utility, flags: .detached) {
+    print("Performing workitem")
+}
+
+//workItem.perform()
+
+let queue = DispatchQueue(label: "Print text", qos: .utility, attributes: .concurrent, autoreleaseFrequency: .workItem, target: DispatchQueue.global(qos: .userInitiated))
+
+queue.asyncAfter(deadline: .now() + 1, execute: workItem)
+
+workItem.notify(queue: .main) {
+    print("workitem is completed!")
+}
+
+workItem.isCancelled
+//workItem.cancel()
+//workItem.isCancelled
+
+//workItem.wait()
+
+//===================
+
+
+
+
+
+
+
+
+
+
+
